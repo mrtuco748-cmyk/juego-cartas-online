2e5c0b60aafad6aa6391a5147ba22f15b3352c88
mrtuco748-cmyk <mrtuco748@gmail.com>
2026-06-14 16:36:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    path: '/socket.io/',
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
  
