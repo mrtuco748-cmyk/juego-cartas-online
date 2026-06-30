2cbf8b0ba730b189e02eefdd34c368bbba6aeafa
mrtuco748-cmyk <mrtuco748@gmail.com>
2026-06-29 21:05:49 -0300

    instalable

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'loop-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/LOOP-icon.svg',
+  '/audio/menu.mp3',
+  '/audio/battle.mp3'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
